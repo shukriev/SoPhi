711226592388c261b58c2fabec024fe6a7c1dff7
shukriev <dark00v3@gmail.com>
2026-07-11 19:10:15 +0300

    feat(sophi-cli): sophi feedback list/delete subcommands

--- sophi-cli/src/main/kotlin/dev/sophi/cli/Main.kt
+++ sophi-cli/src/main/kotlin/dev/sophi/cli/Main.kt
@@ -3,5 +3,9 @@ package dev.sophi.cli
 import com.github.ajalt.clikt.core.subcommands
 
 fun main(args: Array<String>) = SophiCli()
-    .subcommands(McpServeCommand(), LessonsCommand().subcommands(LessonsListCommand(), LessonsArchiveCommand()))
+    .subcommands(
+        McpServeCommand(),
+        LessonsCommand().subcommands(LessonsListCommand(), LessonsArchiveCommand()),
+        FeedbackCommand().subcommands(FeedbackListCommand(), FeedbackDeleteCommand())
+    )
     .main(args)
